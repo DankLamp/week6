89a8d33f7a1cf0a74b850b1db21de837c2958d9b
Dan Lamperd <109046491+DankLamp@users.noreply.github.com>
2023-09-11 23:41:43 +1000

    Update pipepline{.groovy

--- pipepline{.groovy
+++ pipepline{.groovy
@@ -1,26 +1,53 @@
-pipepline{
+pipeline {
     agent any
-    stages{
-        stage("Build"){
-            steps{
-                echo "Building"
-            }
-            post{
-                success{
-                    mail to:"ddlammperd@gmail.com",
-                    subject: "Build Status Email",
-                    body:"Build was successful"
-                }
+    environment {
+        // specifying variables
+        NAME = "Daniel Lamperd"
+        DIRECTORY_PATH = '"C:/ProgramData/Jenkins/.jenkins/jobs/ProjectBeetRoot"'
+        TESTING_ENVIRONMENT = 'TestingServer'
+        
+        // production environment refers to the NAME variable
+        PRODUCTION_ENVIRONMENT = "${env.NAME}"
+    }
+    
+    stages {
+        stage('Build') {
+            steps {
+                echo 'fetch the source code from the directory path specified by the environment variable'
+                echo 'compile the code and generate and necessary artifacts'
             }
         }
-        stage("Test"){
-            steps
-                echo "Testing..."
+        
+        stage('Test') {
+            steps {
+                echo 'units tests'
+                echo 'integration tests'
             }
         }
-        stage("Deploy"){
-            steps{
-                echo "deploying..."
+        
+        stage('Code Quality Check') {
+            steps {
+                echo 'check the quality of the code'
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo 'deployt the application to a testing environment specified by the environment variable'
+            }
+        }
+        
+        stage('Approval') {
+            steps {
+            //waiting 10 seconds to simulate manual approval
+                sleep(time: 10, unit: 'SECONDS')
+            }
+        }
+        
+        stage('Deploy to Production') {
+            steps {
+                echo "${env.NAME}, deploying to production..."
+                echo "deploying code to ${env.PRODUCTION_ENVIRONMENT}"
             }
         }
     }
