f0d782d974b54d5006a5b08fbbb17b162fabe3d8
Dan Lamperd <109046491+DankLamp@users.noreply.github.com>
2023-09-13 15:14:26 +1000

    Update pipepline{.groovy

--- pipepline{.groovy
+++ pipepline{.groovy
@@ -1,54 +1,79 @@
 pipeline {
     agent any
-    environment {
-        // specifying variables
-        NAME = "Daniel Lamperd"
-        DIRECTORY_PATH = '"C:/ProgramData/Jenkins/.jenkins/jobs/ProjectBeetRoot"'
-        TESTING_ENVIRONMENT = 'TestingServer'
-        
-        // production environment refers to the NAME variable
-        PRODUCTION_ENVIRONMENT = "${env.NAME}"
-    }
-    
     stages {
         stage('Build') {
             steps {
-                echo 'fetch the source code from the directory path specified by the environment variable'
-                echo 'compile the code and generate and necessary artifacts'
+                echo 'Building code using Maven'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Cleaning project'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Compiling project'
+                sleep(time: 0.5, unit: 'SECONDS')
             }
         }
-        
-        stage('Test') {
+
+        stage('Unit and Integration tests') {
             steps {
-                echo 'units tests'
-                echo 'integration tests'
+                echo 'Running integration tests using JUnit'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Executing Code'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Testing Assertions'
+                sleep(time: 0.5, unit: 'SECONDS')
             }
         }
-        
-        stage('Code Quality Check') {
+
+        stage('Code Analysis') {
             steps {
-                echo 'check the quality of the code'
+                echo 'Running code analysis using SonarQube'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Scanning Code'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Evaluating Code against quality gate'
+                sleep(time: 0.5, unit: 'SECONDS')
             }
         }
-        
-        stage('Deploy') {
+
+        stage('Security Scan') {
             steps {
-                echo 'deployt the application to a testing environment specified by the environment variable'
+                echo 'Running Security Scan using Nessus'
+                sleep(time: 0.5, unit: 'SECONDS')
+                echo 'Scanning for Vulnerabilities'
+                sleep(time: 0.5, unit: 'SECONDS')
+            }
+            post {
+                success {
+                    // Send a success notification email with logs as attachments
+                    emailext subject: 'Security Scan finished',
+                        body: 'Security scan has succeeded.',
+                        attachLog: true,
+                        to: 'a.abkenar@deakin.edu.au'
+                }
+                failure {
+                    // Send a failure notification email with logs as attachments
+                    emailext subject: 'Security Scan Failed',
+                        body: 'Security scan has failed. Please check the logs for details.',
+                        attachLog: true,
+                        to: 'a.abkenar@deakin.edu.au'
+                }
             }
         }
-        
-        stage('Approval') {
+
+        stage('Integration tests on staging') {
             steps {
-            //waiting 10 seconds to simulate manual approval
-                sleep(time: 10, unit: 'SECONDS')
+                echo 'Running Integration tests on staging environment'
+                sleep(time: 0.5, unit: 'SECONDS')
+                // Include actual steps for integration tests on staging here
             }
         }
-        
+
         stage('Deploy to Production') {
             steps {
-                echo "${env.NAME}, deploying to production..."
-                echo "deploying code to ${env.PRODUCTION_ENVIRONMENT}"
+                echo 'Deploying to production via AWS EC2 instance'
+                sleep(time: 0.5, unit: 'SECONDS')
+                // Include actual deployment steps here
             }
         }
     }
 }
+
